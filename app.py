dfe8d7581e5429bde610247dc6f19137e83ce71d
Drew <notderw@users.noreply.github.com>
2017-10-09 23:34:00 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,199 @@
+import os
+import rethinkdb as db
+import requests
+import json
+
+from datetime import datetime, timezone
+from functools import wraps
+from requests_oauthlib import OAuth2Session
+from flask import Flask, render_template, url_for, redirect, g, request, session, send_from_directory, abort, jsonify
+from flask_wtf.csrf import CSRFProtect
+
+ADMINS = os.environ.get("ADMINS").split(',')
+
+# RETHINKDB
+RETHINKDB_HOST = os.environ.get("RETHINKDB_HOST")
+RETHINKDB_DB = os.environ.get("RETHINKDB_DB")
+RETHINKDB_USER = os.environ.get("RETHINKDB_USER")
+RETHINKDB_PASSWORD = os.environ.get("RETHINKDB_PASSWORD")
+
+# REDDIT API
+REDDIT_APP_ID = os.environ.get("REDDIT_APP_ID")
+REDDIT_APP_SECRET = os.environ.get("REDDIT_APP_SECRET")
+REDDIT_REDIRECT_URI = os.environ.get("REDDIT_REDIRECT_URI")
+
+REDDIT_API_BASE_URL = "https://www.reddit.com/api/v1"
+REDDIT_OAUTH_BASE_URL = "https://oauth.reddit.com/api/v1"
+
+app = Flask(__name__)
+csrf = CSRFProtect(app)
+
+app.debug = True
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
+
+# open connection before each request
+@app.before_request
+def before_request():
+    session.permanent = True
+
+    try:
+        g.db_conn = db.connect(host=RETHINKDB_HOST, port=28015, db=RETHINKDB_DB, user=RETHINKDB_USER, password=RETHINKDB_PASSWORD).repl()
+
+    except db.errors.ReqlDriverError:
+        return render_template('error.html', session=session,  error={
+            'message': 'o fucc this should never happen you should tell someone <br><br> ReqlDriverError'
+        })
+
+# close the connection after each request
+@app.teardown_request
+def teardown_request(exception):
+    try:
+        g.db_conn.close()
+    except AttributeError:
+        pass
+
+def require_auth(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        user = session.get('user', None)
+
+        if not user:
+            return redirect(url_for('login_reddit'))
+
+        if user['id'] not in ADMINS:
+            return 'ur not admin lmao'
+
+        return f(*args, **kwargs)
+    return wrapper
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    now = datetime.utcnow()
+    voting_start = datetime(now.year, now.month % 12 + 1, 1, tzinfo=timezone.utc).timestamp()
+    voting_end = datetime(now.year, now.month, 8, tzinfo=timezone.utc).timestamp()
+
+    if now.timestamp() <= voting_start and now.timestamp() < voting_end:
+        if session.get('user', None) and request.method == 'POST' and request.form.get('vote'):
+            vote = request.form.get('vote')
+            query = db.table('votes').filter({'user_id': session.get('user')['id']})
+
+            if(query.count().run()): query.update({'vote': vote}).run()
+            else: db.table('votes').insert([{'user_id': session.get('user')['id'], 'vote': vote}]).run()
+
+        current_vote = list(db.table('votes').filter({'user_id': session.get('user')['id']}).run()) if session.get('user') else None
+        return render_template('voting.html',
+                                session=session,
+                                admins=ADMINS,
+                                countdown_to=voting_end,
+                                submissions=db.table('submissions').order_by('title').run(),
+                                current_vote=current_vote[0]['vote'] if current_vote else None
+                            )
+
+    else:
+        votes = list(db.table('votes').run())
+        submissions = []
+
+        for submission in db.table('submissions').order_by('title').run():
+            submission['score'] = sum([1 for vote in votes if vote['vote'] == submission['id']])
+            submissions.append(submission)
+
+        return render_template('winner.html',
+                        session=session,
+                        admins=ADMINS,
+                        countdown_to=voting_start,
+                        winner=sorted(submissions, key=lambda k: k['score'], reverse=True)[0]
+                    )
+
+@app.route('/login')
+def login_reddit():
+    # Check for state and for 0 errors
+    state = session.get('oauth2_state')
+    if request.values.get('error'):
+        return render_template('error.html', session=session,  error= {
+            'message': 'There was an error authenticating with reddit: {}'.format(request.values.get('error')),
+            'link': '<a href="{}">Return Home</a>'.format(url_for('verify'))
+        })
+
+    if state and request.args.get('code'):
+        # Fetch token
+        client_auth = requests.auth.HTTPBasicAuth(REDDIT_APP_ID, REDDIT_APP_SECRET)
+        post_data = {"grant_type": "authorization_code", "code": request.args.get('code'), "redirect_uri": REDDIT_REDIRECT_URI}
+        reddit_token = requests.post(REDDIT_API_BASE_URL + "/access_token", auth=client_auth, data=post_data, headers={'User-agent': 'low_poly web auth, /u/RenegadeAI'}).json()
+
+        if not reddit_token or not 'access_token' in reddit_token:
+            return redirect(url_for('logout'))
+
+        # Fetch the user
+        user = requests.get(REDDIT_OAUTH_BASE_URL + "/me", headers={"Authorization": "bearer {}".format(reddit_token["access_token"]), 'User-agent': 'low_poly web, /u/RenegadeAI'}).json()
+
+        session['user'] = {key: user[key] for key in ['name', 'id']}
+
+        return redirect(url_for('home'))
+
+    else:
+        scope = ['identity']
+        reddit = make_reddit_session(scope=scope)
+        authorization_url, state = reddit.authorization_url(
+            REDDIT_API_BASE_URL + "/authorize",
+            access_type="offline"
+        )
+        session['oauth2_state'] = state
+        return redirect(authorization_url)
+
+@app.route('/admin', methods=['GET', 'POST'])
+@require_auth
+def admin():
+    action = request.form.get('action', None)
+    _id = request.form.get('id', None)
+    if request.method == 'POST' and action and _id:
+        if action == 'edit':
+            title = request.form.get('title', None)
+            image = request.form.get('image', None)
+
+            if title and image:
+                db.table('submissions').get(_id).update({'title': title, 'image': image}).run()
+
+            else: return 'Missing required arguments'
+
+        elif action == 'remove':
+            db.table('submissions').get(_id).delete().run()
+
+        else: return 'Undefined action'
+
+    votes = list(db.table('votes').run())
+    submissions = []
+
+    for submission in db.table('submissions').order_by('title').run():
+        submission['score'] = sum([1 for vote in votes if vote['vote'] == submission['id']])
+        submissions.append(submission)
+
+    return render_template('admin.html',
+                            session=session,
+                            submissions=sorted(submissions, key=lambda k: k['score'], reverse=True)
+                        )
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
+
+def to_json(value):
+    return json.dumps(value)
+
+app.jinja_env.filters['to_json'] = to_json
+
+def make_reddit_session(token=None, state=None, scope=None):
+    return OAuth2Session(
+        client_id=REDDIT_APP_ID,
+        token=token,
+        state=state,
+        scope=scope,
+        redirect_uri=REDDIT_REDIRECT_URI,
+        auto_refresh_kwargs={
+            'client_id':None,
+            'client_secret':None,
+        },
+        auto_refresh_url=None,
+        token_updater=None
+    )
---
+++ bot.py
@@ -0,0 +1,348 @@
+import os
+import re
+import praw
+import rethinkdb as db
+import calendar
+import requests
+
+from io import BytesIO
+from PIL import Image
+from datetime import datetime
+from colorcube import ColorCube
+
+class objdict(dict):
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        if name in self:
+            del self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+# RETHINKDB
+RETHINKDB_HOST = os.environ.get("RETHINKDB_HOST")
+RETHINKDB_DB = os.environ.get("RETHINKDB_DB")
+RETHINKDB_USER = os.environ.get("RETHINKDB_USER")
+RETHINKDB_PASSWORD = os.environ.get("RETHINKDB_PASSWORD")
+
+# REDDIT
+REDDIT_CLIENT_ID = os.environ.get("REDDIT_CLIENT_ID")
+REDDIT_CLIENT_SECRET = os.environ.get("REDDIT_CLIENT_SECRET")
+REDDIT_CLIENT_REFRESH_TOKEN = os.environ.get("REDDIT_CLIENT_REFRESH_TOKEN")
+
+db.connect(host=RETHINKDB_HOST, port=28015, db=RETHINKDB_DB, user=RETHINKDB_USER, password=RETHINKDB_PASSWORD).repl()
+
+reddit = praw.Reddit(client_id=REDDIT_CLIENT_ID,
+                     client_secret=REDDIT_CLIENT_SECRET,
+                     refresh_token=REDDIT_CLIENT_REFRESH_TOKEN,
+                     user_agent='/r/low_poly bot by /u/RenegadeAI')
+
+print('Successfully logged into reddit as {}'.format(reddit.user.me()))
+
+subreddit = reddit.subreddit('T7x95PL7')
+
+meta = objdict(list(db.table('meta').run())[0])
+
+now = datetime.utcnow()
+
+monthrange = calendar.monthrange(now.year,now.month)[1]
+month = now.month
+day = 24 # now.day
+
+month_name = calendar.month_name[month]
+time = now.time().replace(microsecond=0)
+
+next_month_name = calendar.month_name[now.month % 12 + 1]
+
+last_month = month % 12 - 1 or 12
+last_month_name = calendar.month_name[last_month]
+last_month_year = now.year - 1 if last_month == 12 else now.year
+
+def update_meta(data):
+    try:
+        db.table('meta').update(data).run()
+
+    except Exception as e:
+        print('Error updating meta {}'.format(e))
+
+def upload_submissions():
+    submissions = [{
+        'image': get_image(submission),
+        'url': submission.shortlink,
+        'title': re.sub("\[[^]]*\]", '', submission.title),
+        'author': submission.author.name
+    } for submission in subreddit.search('flair_css_class:ms+{0}{1}'.format(last_month, str(last_month_year)[2:]), syntax='lucene')]
+
+    db.table('submissions').delete().run()
+    db.table('submissions').insert(submissions).run()
+
+def get_image(submssion):
+    url = submission.url.replace('http:', 'https:')
+
+    match = re.match('(https?)\:\/\/(www\.)?(?:m\.)?imgur\.com/a/([a-zA-Z0-9]+)(#[0-9]+)?', url)
+    if match:
+        album_id = match.group(3)
+
+        response = requests.get('https://api.imgur.com/3/album/{}'.format(album_id), headers={'Authorization': 'Client-ID {}'.format(IMGUR_CLIENT_ID)})
+
+        return response.json()['data']['images'][0]['link']
+
+    match = re.match(r"(?:https?\:\/\/)?(?:www\.)?(?:m\.)?(?:i\.)?imgur\.com\/([a-zA-Z0-9]+)", url)
+    if match:
+        image_id = match.group(1)
+
+        response = requests.get('https://api.imgur.com/3/image/{}'.format(image_id), headers={'Authorization': 'Client-ID {}'.format(IMGUR_CLIENT_ID)})
+
+        return response.json()['data']['link']
+
+    if(url.endswith(tuple(['.png','jpg']))):
+        return url
+
+def update_flairs():
+    subreddit.flair.link_templates.clear()
+
+    def set_flair(label, css_suffix=None):
+        css = '' if 'css' in label else label['text'].lower().replace(' ', '')
+        if css_suffix: css += ' ' + css_suffix
+        if 'editable' not in label: label['editable'] = False
+
+        subreddit.flair.link_templates.add(label['text'], css_class=css, text_editable=label['editable'])
+
+    labels = [
+        { 'text': 'Blender' },
+        { 'text': 'Unity' },
+        { 'text': 'Modo' },
+        { 'text': '3DS Max' },
+        { 'text': 'Cinema 4D' },
+        { 'text': 'Maya' },
+        { 'text': '<other>', 'css': False, 'editable': True }
+    ]
+
+    for label in labels:
+        set_flair(label)
+
+    for label in labels:
+        set_flair(label, css_suffix = 'ms {0}{1}'.format(month, str(now.year)[2:]))
+
+def get_monthly_theme():
+        submission = reddit.submission(meta.theme_voting)
+        submission.comment_sort = 'top'
+
+        return sorted(submission.comments, key=lambda k: k.score, reverse=True)[0]
+
+def get_winner():
+    votes = list(db.table('votes').run())
+    submissions = []
+    for submission in db.table('submissions').order_by('title').run():
+            submission['score'] = sum([1 for vote in votes if vote['vote'] == submission['id']])
+            submissions.append(submission)
+
+    return objdict(sorted(submissions, key=lambda k: k['score'], reverse=True)[0])
+
+def substitute_content(original_content, new, marker):
+    content = re.sub(r'(\[\]\(#' + marker + '\)).*(\[\]\(/' + marker + '\))', '\\1\\2', original_content, flags=re.DOTALL)
+    opening_marker = "[](#" + marker + ")"
+
+    try:
+        marker_pos = content.index(opening_marker) + len(opening_marker)
+        return content[:marker_pos] + new + content[marker_pos:]
+
+    except ValueError:
+        # Substring not found
+        print("Marker {} not found in content".format(opening_marker))
+        return original_content
+
+def clamp(val, minimum=0, maximum=255):
+    if val < minimum:
+        return minimum
+    if val > maximum:
+        return maximum
+    return val
+
+def colorscale(hexstr, scalefactor):
+    """
+    Scales a hex string by ``scalefactor``. Returns scaled hex string.
+
+    To darken the color, use a float value between 0 and 1.
+    To brighten the color, use a float value greater than 1.
+
+    >>> colorscale("#DF3C3C", .5)
+    #6F1E1E
+    >>> colorscale("#52D24F", 1.6)
+    #83FF7E
+    >>> colorscale("#4F75D2", 1)
+    #4F75D2
+    """
+
+    hexstr = hexstr.strip('#')
+
+    if scalefactor < 0 or len(hexstr) != 6:
+        return hexstr
+
+    r, g, b = int(hexstr[:2], 16), int(hexstr[2:4], 16), int(hexstr[4:], 16)
+
+    r = clamp(r * scalefactor)
+    g = clamp(g * scalefactor)
+    b = clamp(b * scalefactor)
+
+    return "#%02x%02x%02x" % (int(r), int(g), int(b))
+
+def update_theme(winner):
+    new_sidebar = substitute_content(subreddit.mod.settings()['description'],
+                                    (
+                                       ""
+                                       "> * {0} monthly winner:*"
+                                       "[{1.title:.30}]({2}) by [{1.author}]({1.author})"
+                                       ""
+                                    ).format(last_month_name, winner, requests.utils.unquote(winner.url)),
+                                    'BOTWINNER'
+                                )
+
+    subreddit.mod.update(description=new_sidebar)
+
+    #################################################################
+    # Download the winner image, resize it, and upload it to reddit
+    #################################################################
+
+    response = requests.get(requests.utils.unquote(winner.image), stream=True, headers={'User-agent': 'Mozilla/5.0'})
+    response.raw.decode_content = True
+
+    header_image = Image.open(response.raw).convert('RGB')
+
+    ratio = 1920 / header_image.size[0]
+
+    header_image = header_image.resize((int(header_image.size[0] * ratio), header_image.size[1] * 1), Image.ANTIALIAS)
+    top = int(header_image.size[1] / 2) - 260
+    header_image = header_image.crop((0, top, 1920, top + 416))
+
+    # If we just use im.tostring() we get a massive image that won't upload
+    def toJPEG(im):
+        with BytesIO() as f:
+            im.save(f, format='JPEG')
+            return f.getvalue()
+
+    # PRAW doesn't support raw image uploads so we will do it ourselves
+    # https://github.com/praw-dev/praw/blob/a75ebcf934fb49a6966a04d172fa00e957836958/praw/models/reddit/subreddit.py#L1874
+    url = praw.const.API_PATH['upload_image'].format(subreddit=subreddit.display_name)
+    subreddit._reddit.post(url,
+                           data={
+                               'name': 'headerimg',
+                               'upload_type': 'img',
+                               'img_type': 'jpg'
+                           },
+                           files={'file': toJPEG(header_image)})
+
+    #################################################################
+    #Calulate colors, and upload the custom stylesheet bit
+    #################################################################
+
+    cc = ColorCube(avoid_color=[255, 255, 255], distinct_threshold=0.8)
+    image = header_image.resize((50, 50))
+    primary = '#%02x%02x%02x' % tuple(cc.get_colors(image)[0])
+
+    bot_style = (
+        ".side .titlebox .md h3 a, .drop-choices a.choice:hover, .submit-page #newlink.submit.content ul.tabmenu.formtab,"
+        ".submit_text.enabled.roundfield, body .btn, body button, .content .infobar,"
+        "form input[type=checkbox]:checked + label:before, .pretty-form input[type=checkbox]:checked + label:before,"
+        ".titlebox .fancy-toggle-button .active.add,"
+        ".reddit-infobar.with-icon.locked-infobar, .reddit-infobar.with-icon.locked-infobar:before,"
+        ".flair, .side .md>blockquote:first-of-type a:hover:after {{background-color: {color.primary}}}"
+
+        ".side .titlebox .md h3 a:hover, .btn:hover, body button:hover, .titlebox .fancy-toggle-button .active.add:hover {{background-color: {color.hover};}}"
+
+        ".side .titlebox .md h3 a:active , .btn:active , body button:active, .titlebox .fancy-toggle-button .active.add:active {{background-color: {color.active};}}"
+
+        ".thing .title.loggedin.click, .thing .title.click, .thing .title.loggedin, .thing .title, .link .entry .buttons li a.comments,"
+        ".link .entry .buttons li a.flairselectbtn, .link .entry .buttons li a:hover, .titlebox .tagline a.flairselectbtn, .md a,"
+        ".side .titlebox .md h4 a, .wiki-page .wiki-page-content .md.wiki h4, .sidebox.create .morelink a, a, .side:after, .usertext .bottom-area a.reddiquette,"
+        ".wiki-page .pageactions .wikiaction-current, .tagline .submitter, .combined-search-page .search-result .search-result-header .search-title, "
+        ".combined-search-page .search-result a, .combined-search-page .search-result a>mark, .combined-search-page .search-result .search-comments, "
+        ".flairselector h2, .linefield .title, body .content .sitetable .link .title a:hover, .link .entry .tagline a:hover, .comment .author:hover,"
+        ".morelink a, .morelink:hover a, #header .tabmenu li.selected a, form input[type=checkbox]:checked + label, .pretty-form input[type=checkbox]:checked + label, .side .md>blockquote:first-of-type a {{color: {color.primary};}}"
+
+        "form input[type=checkbox]:checked + label:before, .pretty-form input[type=checkbox]:checked + label:before {{ border-color: {color.primary};}}"
+
+        "body .content .roundfield textarea:focus,  body .content input[type=text]:focus, body .content input[type=url]:focus, .roundfield input[type=password]:focus, .roundfield input[type=number]:focus {{ border-color: {color.primary}; box-shadow: 0 1px 0 0 {color.primary};}}"
+        ).format(color = objdict({
+                                'primary': primary,
+                                'hover': colorscale(primary, 1.1),
+                                'active': colorscale(primary, .75),
+                            }))
+
+    stylesheet = substitute_content(subreddit.stylesheet().stylesheet,
+                                    bot_style,
+                                    'POLYGONAUTOMATON'
+                                )
+
+    subreddit.stylesheet.update(stylesheet)
+
+def main():
+    if(day == monthrange - 7):
+        reddit.submission(meta.monthly_winner).mod.sticky(state=False)
+
+        submission = subreddit.submit("{} theme voting".format(next_month_name),
+                                      selftext = ("Hello everyone, please comment your suggestion for next months theme.\n\n"
+                                                  "The deadline for voting will be the end of {0} {1}.\n\n"
+                                                  "Please limit your response to only your theme idea, I am not a smart bot."
+                                                 ).format(month_name, monthrange)
+                          )
+
+        submission.mod.contest_mode()
+        submission.mod.sticky(bottom=True)
+
+        update_meta({'theme_voting': submission.id})
+
+    elif(day == 1):
+        reddit.submission(meta.theme).mod.sticky(state=False)
+        reddit.submission(meta.theme_voting).mod.sticky(state=False)
+        reddit.submission(meta.theme_voting).mod.lock()
+
+        upload_submissions()
+
+        submission = subreddit.submit("{} voting now open! Click here to pick your favourite submission!".format(last_month_name),
+                                      url = "https://lowpoly.synesis.co/"
+                                      )
+        submission.mod.sticky()
+
+        update_meta({'voting': submission.id})
+
+        winner_comment = get_monthly_theme()
+
+        submission = subreddit.submit("{0} monthly theme: {1}".format(month_name, winner_comment.body),
+                                      selftext = (
+                                          "This months theme is {0.body} as suggested by /u/{0.author}\n"
+                                          "\n"
+                                          "Submissions will be due at the end of the month"
+                                          ).format(winner_comment)
+                                      )
+        submission.mod.sticky(bottom=True)
+
+        update_meta({'theme': submission.id})
+
+        update_flairs()
+
+    elif(day == 8):
+        winner = get_winner()
+
+        submission = subreddit.submit("{0} monthly winner: {1}".format(last_month_name, winner.author),
+                                      selftext = (
+                                              "Thanks to everyone who participated in last month's challenge.\n"
+                                              "\n"
+                                              "{0}'s winner is /u/{1.author}, with their submission: [{1.title}]({2})"
+                                          ).format(last_month_name, winner, requests.utils.unquote(winner.url))
+                                     )
+        submission.mod.sticky()
+
+        update_meta({'monthly_winner': submission.id})
+
+        update_theme(winner)
+
+if __name__ == '__main__':
+    main()
